a248d7c98a2a92af8c8427076fbe743772a04aa3
Hwang sin phil <tlsvlfghkds@gmail.com>
2020-11-11 17:46:08 +0900

    server
    server basic

---
+++ BombRect/BombRect_Server/BoomRectServer.cpp
@@ -0,0 +1,110 @@
+#include "S_global.h"
+#include "S_packet.h"
+
+void error_quit(char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+
+}
+
+void error_display(char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+
+
+//이용해서 %출력
+int recvn(SOCKET s, char* buf, int len, int flags) {
+	int received;
+	char* ptr = buf;
+	int left = len;
+
+	while (left > 0) {
+		received = recv(s, ptr, left, flags);
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+		else if (received == 0)
+			break;
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+
+int main(int argc, char* argv[])
+{
+	int retval;
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	//socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) error_quit("socket()");
+
+	//bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) error_quit("bind()");
+
+	//listen()
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) error_quit("listen()");
+	HANDLE hThread;
+	cout << "listen\n";
+
+	//데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE];
+	login_packet::CS_Nickname nicknamePacket;
+	while (1) {
+		//accept()
+
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+		cout << "accept\n";
+		if (client_sock == INVALID_SOCKET) {
+			error_display((char*)"accept()");
+			break;
+		}
+		
+		retval = recv(client_sock, buf, strlen((const char*)nicknamePacket.buf), 0);
+
+
+
+
+	}
+
+
+
+	closesocket(client_sock);
+	closesocket(listen_sock);
+
+	WSACleanup();
+
+}
---
+++ BombRect/BombRect_Server/S_global.h
@@ -0,0 +1,29 @@
+#pragma once
+#pragma comment(lib,"ws2_32")
+
+#include <WinSock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include<iostream>
+#include<process.h>
+#include <mutex>
+#define SERVERPORT 9000
+#define BUFSIZE	   512
+using namespace std;
+
+void error_quit(char* msg);
+
+void error_display(char* msg);
+
+int recvn(SOCKET, char*, int, int);
+
+
+
+enum class SceneID : int {
+	LOGIN,
+	LOBBY,
+	GAME,
+	RESULT,
+	COUNT,
+};
